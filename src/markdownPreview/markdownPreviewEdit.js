9a8d67b134800dcf3ecaf463c6ffdb2d7c2ecca0
Simon Klein <simon.klein@cloudogu.com>
2021-06-17 07:39:32 +0200

    #1 Add tooltips

--- src/markdownPreview/markdownPreviewEdit.js
+++ src/markdownPreview/markdownPreviewEdit.js
@@ -1,9 +1,15 @@
 'use strict';
 
 angular.module('adf.widget.scm')
-  .controller('MarkdownPreviewEditController', function (repositories, SCM) {
+  .controller('MarkdownPreviewEditController', function (repositories, SCM, $scope, $sce) {
     var vm = this;
     vm.repositories = repositories;
+
+    // tooltip for project
+    $scope.pathToMarkdownFileTooltip = $sce.trustAsHtml('The path where the Markdown file can be found within the repository (e.g. "docs/File.md").');
+    $scope.branchesTooltip = $sce.trustAsHtml('Select the branch where the desired Markdown file can be found.');
+    $scope.repositoryTooltip = $sce.trustAsHtml('Select the repository where the desired Markdown file can be found.');
+
 
     vm.getBranchesByRepositoryId = function (repositoryId) {
       if (repositoryId) {
