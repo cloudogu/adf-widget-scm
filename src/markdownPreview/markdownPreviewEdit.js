50648b00af92d3823a961b2dcec060e0cd42b48e
jbartscher <jasper.bartscher@cloudogu.com>
2021-06-23 09:04:30 +0200

    Fix placement and display of markdown tooltip

--- src/markdownPreview/markdownPreviewEdit.js
+++ src/markdownPreview/markdownPreviewEdit.js
@@ -5,16 +5,16 @@ angular.module('adf.widget.scm')
     var vm = this;
     vm.repositories = repositories;
 
-    // tooltip for project
-    $scope.pathToMarkdownFileTooltip = $sce.trustAsHtml('The path where the Markdown file can be found within the repository (e.g. <b>docs/File.md</b>).');
-    $scope.branchesTooltip = $sce.trustAsHtml('Select the branch on which the desired Markdown file can be found.');
+    // tooltips
     $scope.repositoryTooltip = $sce.trustAsHtml('Select the repository which stores the desired Markdown.');
+    $scope.branchTooltip = $sce.trustAsHtml('Select the branch on which the desired Markdown file can be found.');
+    $scope.pathToMarkdownTooltip = $sce.trustAsHtml('The path where the Markdown file can be found within the repository (e.g. <b>docs/File.md</b>).');
 
     vm.getBranchesByRepositoryId = function (repositoryId) {
       if (repositoryId) {
         SCM.getBranchesByRepositoryId(repositoryId).then(function (result) {
           // catch repositories without branch support
-          if (result.status == 400)  {
+          if (result.status === 400)  {
             vm.branches = null;
           }else{
             vm.branches = result.branch;
